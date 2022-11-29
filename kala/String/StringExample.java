23b7dcb7fcc9472c5cef558fdd77a50316042e0f
Kalavalli99 <75485779+Kalavalli99@users.noreply.github.com>
2022-11-29 10:13:09 +0530

    Create StringExample.java

---
+++ kala/String/StringExample.java
@@ -0,0 +1,14 @@
+package String;
+
+public class StringEx {
+
+	public static void main(String[] args) {
+		
+		String greet = "Hello! World";
+		System.out.println("String: " + greet);
+		
+		int length = greet.length();
+		System.out.println("Length: " + length);
+	}
+
+}
