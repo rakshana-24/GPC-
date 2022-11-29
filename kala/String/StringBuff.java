d8abe173d9bce39f8246dd783e75593faabaeb17
Kalavalli99 <75485779+Kalavalli99@users.noreply.github.com>
2022-11-29 10:12:46 +0530

    Create StringBuff.java

---
+++ kala/String/StringBuff.java
@@ -0,0 +1,11 @@
+package String;
+
+public class StringBuff {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("Hello");
+		sb.append("Java");
+		System.out.println(sb);
+	}
+
+}
