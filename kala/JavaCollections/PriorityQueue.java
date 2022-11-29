84d1c6f97734d746e3a43540c340b61815e6d15c
Kalavalli99 <75485779+Kalavalli99@users.noreply.github.com>
2022-11-29 10:05:16 +0530

    Create PriorityQueue.java

---
+++ kala/JavaCollections/PriorityQueue.java
@@ -0,0 +1,20 @@
+package JavaCollections;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityQueueDemo {
+	public static void main(String[] args) {
+		
+		Queue<Integer> q = new PriorityQueue<>();
+		System.out.println(q.peek());
+		//System.out.println(q.element());
+		
+		for (int i =20; i <= 30; i++) {
+			q.offer(i);
+		}
+		
+		System.out.println(q);
+		System.out.println(q.poll());
+	}
+}
