6684480bbb5b00c9afc779819a67b7f801b33eae
Kalavalli99 <75485779+Kalavalli99@users.noreply.github.com>
2022-11-29 10:10:15 +0530

    Create Multilevel.java

---
+++ kala/OOPS/Multilevel.java
@@ -0,0 +1,27 @@
+package OOPS;
+
+class Shape{
+	public void display() {
+		System.out.println("Inside display");
+	}
+}
+class Rectangle extends Shape {
+	public void area() {
+		System.out.println("Inside area");
+	
+	}
+}
+class Cube extends Rectangle {
+	public void volume() {
+		System.out.println("Inside volume");
+	}
+}
+public class Multilevel {
+	public static void main(String[] arguments) {
+		Cube cube = new Cube();
+		cube.display();
+		cube.area();
+		cube.volume();
+
+		}
+}
