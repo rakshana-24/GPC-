64cbfcf81e48a8a40a9f4accac85b160e25d90f2
Kalavalli99 <75485779+Kalavalli99@users.noreply.github.com>
2022-11-29 10:06:48 +0530

    Create NestedLoop.java

---
+++ kala/loops/NestedLoop.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class nestedLoop {
+	public static void main(String[] args) {
+		int weeks = 3;
+		int days = 7;
+		
+		for (int i = 1; i <= weeks; ++i) {
+			System.out.println("Week: " + i);
+			
+		for (int j = 1; j <= days; ++j) {
+			System.out.println("Days: " + j);
+		}
+		}
+	}
+}
